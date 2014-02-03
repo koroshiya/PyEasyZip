d3e458ce43af72755f4f61c38912cc73a9e628cf
koroshiya <korocresent@gmail.com>
2014-02-03 14:27:02 +1100

    Turned ZipCLI into a class so it can be used externally

--- ZipCLI.py
+++ ZipCLI.py
@@ -1,119 +1,126 @@
 #!/usr/bin/python2.7 -tt
 
-import zipfile
 import sys
+
+if sys.version_info < (2, 7):
+    print ("Must use python 2.7 or greater\n")
+    sys.exit()
+elif sys.version_info[0] > 2:
+	print ("Incompatible with Python 3\n")
+	sys.exit()
+
+import zipfile
 import os
 
-overwrite    = False 
-topLevelOnly = False
-encompassAll = False
-verbose      = False
-incArchives  = False
+class ZipCLI():
 
-archiveFormats = ['zip','rar','tar','gz','xz','ar','bz2','7z','cbr','cbz']
+	def __init__(self, *args, **kwargs):
+		self.overwrite    = False 
+		self.topLevelOnly = False
+		self.encompassAll = False
+		self.verbose      = False
+		self.incArchives  = False
 
-def printVerbose(*args):
-	if verbose:
-		argList = ''
-		for arg in args:
-			argList += arg + ' '
-		print argList
+		self.archiveFormats = ['zip','rar','tar','gz','xz','ar','bz2','7z','cbr','cbz']
 
-def processParam(arg):
-	if arg[0] == '-':
-		for param in arg[1:]:
-			if param == 'o':
-				global overwrite
-				overwrite = True
-			elif param == 't':
-				global topLevelOnly
-				topLevelOnly = True
-			elif param == 'a':
-				global encompassAll
-				encompassAll = True
-			elif param == 'v':
-				global verbose
-				verbose = True
-			elif param == 'z':
-				global incArchives
-				incArchives = True
-			else:
-				pass
-		return True
-	else:
-		return False
+	def printVerbose(self, *args):
+		if self.verbose:
+			argList = ''
+			for arg in args:
+				argList += arg + ' '
+			print argList
 
-def writeDirToZipRecursively(arg):
-	fileList = []
-	nameList = []
-	for root, dirs, files in os.walk(arg):
-		for f in files:
-			fileList.append(root + '/' + f)
-			nameList.append(f)
-		for d in dirs:
-			arrs = writeDirToZipRecursively(d)
-			for f in arrs[0]:
-				fileList.append(f)
-			for n in arrs[1]:
-				nameList.append(n)
-	return [fileList, nameList]
-
-def zipDir(arg, dirs, nfiles):
-	names = []
-	files = []
-	for f in nfiles:
-		files.append(arg + '/' + f)
-		names.append(f)
-	zipDirDirectly(arg, files, names)
-
-def zipDirDirectly(arg, files, names):
-	zipName = arg + '.zip';
-	if os.path.isfile(zipName):
-		if overwrite:
-			if not os.access(os.path.dirname(arg), os.W_OK):
-				printVerbose('Cannot delete file:', zipName)
-				return
-			else:
-				os.remove(zipName)
+	def processParam(self, arg):
+		if arg[0] == '-':
+			for param in arg[1:]:
+				if param == 'o':
+					self.overwrite = True
+				elif param == 't':
+					self.topLevelOnly = True
+				elif param == 'a':
+					self.encompassAll = True
+				elif param == 'v':
+					self.verbose = True
+				elif param == 'z':
+					self.incArchives = True
+				else:
+					pass
+			return True
 		else:
-			printVerbose(zipName, 'already exists')
+			return False
+
+	def writeDirToZipRecursively(self, arg):
+		fileList = []
+		nameList = []
+		for root, dirs, files in os.walk(arg):
+			for f in files:
+				fileList.append(root + '/' + f)
+				nameList.append(f)
+			for d in dirs:
+				arrs = self.writeDirToZipRecursively(d)
+				for f in arrs[0]:
+					fileList.append(f)
+				for n in arrs[1]:
+					nameList.append(n)
+		return [fileList, nameList]
+
+	def zipDir(self, arg, dirs, nfiles):
+		names = []
+		files = []
+		for f in nfiles:
+			files.append(arg + '/' + f)
+			names.append(f)
+		self.zipDirDirectly(arg, files, names)
+
+	def zipDirDirectly(self, arg, files, names):
+		zipName = arg + '.zip';
+		if os.path.isfile(zipName):
+			if self.overwrite:
+				if not os.access(os.path.dirname(arg), os.W_OK):
+					self.printVerbose('Cannot delete file:', zipName)
+					return
+				else:
+					os.remove(zipName)
+			else:
+				self.printVerbose(zipName, 'already exists')
+				return
+		zip = zipfile.ZipFile(zipName, 'w', compression=zipfile.ZIP_DEFLATED)
+		for i in range(len(files)):
+			if self.incArchives or not '.' in names[i] or '.' in names[i] and not names[i].split(".")[-1] in self.archiveFormats:
+				zip.write(files[i], names[i])
+			else:
+				self.printVerbose('Skipping archive file',names[i])
+		zip.close()
+		self.printVerbose('Created zip file ' + os.path.basename(os.path.normpath(arg)) + '.zip')
+
+	def processDir(self, arg):
+		self.printVerbose('argument',arg)
+		for root, dirs, files in os.walk(arg):
+			if self.topLevelOnly:
+				if self.encompassAll:
+					arrs = self.writeDirToZipRecursively(arg)
+					self.zipDirDirectly(arg, arrs[0], arrs[1])
+				else:
+					self.zipDir(arg, dirs, files)
+			elif len(dirs) > 0:
+				for adir in dirs:
+					self.processDir(arg + '/' + adir)
+			else:
+				if not os.access(arg, os.R_OK):
+					self.printVerbose('Cannot read from', arg)
+					return
+				elif not os.access(os.path.dirname(arg), os.W_OK):
+					self.printVerbose('Cannot write to', os.path.dirname(arg))
+					return
+				self.zipDir(arg, dirs, files)
 			return
-	zip = zipfile.ZipFile(zipName, 'w', compression=zipfile.ZIP_DEFLATED)
-	for i in range(len(files)):
-		if incArchives or not '.' in names[i] or '.' in names[i] and not names[i].split(".")[-1] in archiveFormats:
-			zip.write(files[i], names[i])
-		else:
-			printVerbose('Skipping archive file',names[i])
-	zip.close()
-	printVerbose('Created zip file ' + os.path.basename(os.path.normpath(arg)) + '.zip')
-
-def processDir(arg):
-	printVerbose('argument',arg)
-	for root, dirs, files in os.walk(arg):
-		if topLevelOnly:
-			if encompassAll:
-				arrs = writeDirToZipRecursively(arg)
-				zipDirDirectly(arg, arrs[0], arrs[1])
-			else:
-				zipDir(arg, dirs, files)
-		elif len(dirs) > 0:
-			for adir in dirs:
-				processDir(arg + '/' + adir)
-		else:
-			if not os.access(arg, os.R_OK):
-				printVerbose('Cannot read from', arg)
-				return
-			elif not os.access(os.path.dirname(arg), os.W_OK):
-				printVerbose('Cannot write to', os.path.dirname(arg))
-				return
-			zipDir(arg, dirs, files)
-		return
 
 if len(sys.argv) > 1:
+	gui = ZipCLI()
 	for arg in sys.argv:
-		if processParam(arg):
+		if gui.processParam(arg):
 			pass
 		elif os.path.isdir(arg):
-			processDir(arg)
+			gui.processDir(arg)
 else:
 	print "No files specified"
