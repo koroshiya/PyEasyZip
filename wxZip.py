915f637d277bd8b7d9e7ec7c23022069dd863503
koroshiya <korocresent@gmail.com>
2014-02-03 15:28:31 +1100

    Added CLI args as checkboxes for Options menu

--- wxZip.py
+++ wxZip.py
@@ -21,8 +21,13 @@ except ImportError:
 import os
 import ZipCLI
 
-FILE_OPEN_DIRECTORY = 650
+PARSE_ARGS = 650
 FILE_CLOSE = 666
+OPTIONS_OVERWRITE = 670
+OPTIONS_TOP_LEVEL = 671
+OPTIONS_ENCOMPASS_ALL = 672
+OPTIONS_VERBOSE = 673
+OPTIONS_INCLUDE_ARCHIVES = 674
 INIT_TEXT = 'Drag files here to begin zipping'
 
 class wxGUI(wx.Frame):
@@ -50,14 +55,22 @@ class wxGUI(wx.Frame):
 			self.statusbar.SetStatusText('Must enter at least one folder to zip')
 			return
 		data = self.textarea.GetValue().splitlines()
+
+		cli = ZipCLI.ZipCLI()
+		cli.overwrite = self.tOverwrite.IsChecked()
+		cli.topLevelOnly = self.tTopLevel.IsChecked()
+		cli.encompassAll = self.tEncompass.IsChecked()
+		cli.verbose = self.tVerbose.IsChecked()
+		cli.incArchives = self.tIncludeArchives.IsChecked()
+
 		for i in reversed(xrange(0,len(data))):
-			self.parseArg(data[i])
+			self.parseArg(data[i], cli)
 			data.pop()
 			self.textarea.SetValue('\n'.join(data))
+		self.textarea.SetValue(INIT_TEXT)
 		self.statusbar.SetStatusText('Finished')
 	
-	def parseArg(self, arg):
-		cli = ZipCLI.ZipCLI()
+	def parseArg(self, arg, cli):
 		if os.path.isdir(arg):
 			self.statusbar.SetStatusText('Parsing ' + arg)
 			cli.processDir(arg)
@@ -76,14 +89,27 @@ class wxGUI(wx.Frame):
 		self.menubar = wx.MenuBar()
 		menuFile = wx.Menu()
 		menuParse = wx.Menu()
+		menuOptions = wx.Menu()
 
 		#menuFile.AppendSeparator()
 		self.SetMenuItem(menuFile, FILE_CLOSE, '&Quit\tCtrl+Q', self.Exit)
 
-		self.SetMenuItem(menuParse, FILE_OPEN_DIRECTORY, '&Parse\tCtrl+P', self.parseArgs)
+		self.SetMenuItem(menuParse, PARSE_ARGS, '&Parse\tCtrl+P', self.parseArgs)
+
+		self.tOverwrite = menuOptions.Append(OPTIONS_OVERWRITE, '&Overwrite\tCtrl+O', 
+											'Overwrite zip file if it already exists', kind=wx.ITEM_CHECK)
+		self.tTopLevel = menuOptions.Append(OPTIONS_TOP_LEVEL, '&Top Level Only\tCtrl+T', 
+											'Only zip the specified directory, not its subdirectories', kind=wx.ITEM_CHECK)
+		self.tEncompass = menuOptions.Append(OPTIONS_ENCOMPASS_ALL, '&Encompass All\tCtrl+A', 
+											'Include files from subdirectories when only zipping the top level', kind=wx.ITEM_CHECK)
+		self.tVerbose = menuOptions.Append(OPTIONS_VERBOSE, '&Verbose\tCtrl+V', 
+											'Displays more verbose logging if started from the command line', kind=wx.ITEM_CHECK)
+		self.tIncludeArchives = menuOptions.Append(OPTIONS_INCLUDE_ARCHIVES, '&Include Archives\tCtrl+Z', 
+											'Zip archives found within the directory specified', kind=wx.ITEM_CHECK)
 
 		self.menubar.Append(menuFile, '&File')
 		self.menubar.Append(menuParse, '&Parse')
+		self.menubar.Append(menuOptions, '&Options')
 
 		self.statusbar = self.CreateStatusBar()
 		self.SetMenuBar(self.menubar);
@@ -92,6 +118,7 @@ class wxGUI(wx.Frame):
 		mItem = wx.MenuItem(menu, idn, text)
 		menu.AppendItem(mItem)
 		self.Bind(wx.EVT_MENU, event, id=idn)
+		return mItem
 
 	def Exit(self, e):
 		wx.Exit()
